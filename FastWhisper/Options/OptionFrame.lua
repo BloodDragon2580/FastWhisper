278305a277f19d3950f80d97f8e1a656e510bbd2
BloodDragon2580 <gewinner.thomas@gmail.com>
2024-08-16 08:48:34 +0200

    'Update'

--- FastWhisper/Options/OptionFrame.lua
+++ FastWhisper/Options/OptionFrame.lua
@@ -13,9 +13,26 @@ BINDING_NAME_FASTWHISPER_TOGGLE = L["toggle frame"]
 
 -- Initialisiere optionFrame vor der Verwendung
 local frame = CreateFrame("Frame", "FastWhisperOptionFrame", UIParent, "BasicFrameTemplateWithInset")
-frame:SetSize(300, 400) -- Beispielgröße, anpassen nach Bedarf
+frame:SetSize(400, 600) -- Vergrößerte Größe des Frames
 frame:SetPoint("CENTER")
 frame:Hide() -- Initial ausblenden
+
+-- Funktion zum Verschieben des Frames
+local function StartMoving(self)
+    self:StartMoving()
+end
+
+local function StopMovingOrSizing(self)
+    self:StopMovingOrSizing()
+    local point, _, _, x, y = self:GetPoint()
+    addon.db.framePosition = {point, x, y}
+end
+
+frame:SetMovable(true)
+frame:EnableMouse(true)
+frame:RegisterForDrag("LeftButton")
+frame:SetScript("OnDragStart", StartMoving)
+frame:SetScript("OnDragStop", StopMovingOrSizing)
 
 addon.optionFrame = frame
 
@@ -24,10 +41,11 @@ function addon.optionFrame:Open()
     self:Show()
 end
 
+-- Konfigurationsbutton für das Hauptfenster
 local configButton = CreateFrame("Button", addon.frame:GetName().."Config", addon.frame, "UIPanelButtonTemplate")
 configButton:SetSize(16, 16)
 configButton:SetNormalTexture("Interface\\Icons\\Trade_Engineering")
-configButton:SetPoint("TOPLEFT", 11, -9)
+configButton:SetPoint("TOPLEFT", 10, -10)
 
 configButton:SetScript("OnEnter", function(self)
     GameTooltip:SetOwner(self, "ANCHOR_LEFT")
@@ -50,15 +68,16 @@ configButton:SetScript("OnClick", function(self)
     end
 end)
 
+-- Titel für allgemeine Optionen
 local generalGroup = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
 generalGroup:SetText(L["general options"])
-generalGroup:SetPoint("TOPLEFT", frame, "TOPLEFT", 10, -10)
+generalGroup:SetPoint("TOPLEFT", frame, "TOPLEFT", 10, -40) -- Titel positioniert
 
--- Funktion um Schaltflächen hinzuzufügen
+-- Funktion zum Hinzufügen von Schaltflächen
 local function AddButton(group, text, key)
     local button = CreateFrame("CheckButton", nil, frame, "UICheckButtonTemplate")
     button.text:SetText(text)
-    button:SetPoint("TOPLEFT", group, "BOTTOMLEFT", 0, -5)
+    button:SetPoint("TOPLEFT", group, "BOTTOMLEFT", 0, -25) -- Abstand zwischen Buttons angepasst
     button:SetScript("OnClick", function(self)
         local checked = self:GetChecked()
         addon.db[key] = checked
@@ -67,7 +86,6 @@ local function AddButton(group, text, key)
             addon:PlaySound()
         end
     end)
-    group = button
     return button
 end
 
@@ -78,11 +96,7 @@ local soundButton = AddButton(receiveOnlyButton, L["sound notify"], "sound")
 local realmCheck = AddButton(soundButton, L["show realms"], "showRealm")
 
 local foreignCheck = AddButton(realmCheck, L["foreign realms"], "foreignOnly")
-foreignCheck:SetPoint("TOPLEFT", realmCheck, "BOTTOMLEFT", 0, -5)
-
 local timeCheck = AddButton(foreignCheck, L["timestamp"], "time")
-timeCheck:SetPoint("TOPLEFT", foreignCheck, "BOTTOMLEFT", 0, -5)
-
 local ignoreTagsButton = AddButton(timeCheck, L["ignore tag messages"], "ignoreTags")
 local applyFiltersButton = AddButton(ignoreTagsButton, L["apply third-party filters"], "applyFilters")
 local saveButton = AddButton(applyFiltersButton, L["save messages"], "save")
@@ -95,6 +109,7 @@ addon:RegisterOptionCallback("showRealm", function(value)
     end
 end)
 
+-- Funktionen für Slider
 local function Slider_OnSliderInit(self)
     return addon.db[self.key]
 end
@@ -112,29 +127,31 @@ local function CreateSlider(text, key, fmt)
     slider.text = slider:CreateFontString(nil, "ARTWORK", "GameFontNormal")
     slider.text:SetText(text)
     slider.text:SetPoint("BOTTOM", slider, "TOP", 0, 0)
-    slider:SetPoint("TOPLEFT", frame, "TOPLEFT", 20, -20) -- Beispielposition, anpassen nach Bedarf
+    slider:SetSize(320, 20) -- Größe des Sliders
+    slider:SetPoint("TOPLEFT", frame, "TOPLEFT", 20, -220) -- Positionierung der Slider
     slider.key = key
     slider.OnSliderInit = Slider_OnSliderInit
     slider.OnValueChanged = Slider_OnSliderChanged
     return slider
 end
 
+-- Titel für Frame-Einstellungen
 local frameLabel = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
 frameLabel:SetText(L["frame settings"])
-frameLabel:SetPoint("TOPLEFT", saveButton, "BOTTOMLEFT", 0, -16)
+frameLabel:SetPoint("TOPLEFT", frame, "TOPLEFT", 20, -280) -- Titelpositionierung angepasst
 
 local notifySlider = CreateSlider(L["button scale"], "buttonScale", "%d%%")
-notifySlider:SetPoint("TOPLEFT", frameLabel, "BOTTOMLEFT", 8, -30)
-
 local mainSlider = CreateSlider(L["list scale"], "listScale", "%d%%")
-mainSlider:SetPoint("LEFT", notifySlider, "RIGHT", 24, 0)
-
 local widthSlider = CreateSlider(L["list width"], "listWidth")
-widthSlider:SetPoint("TOPLEFT", notifySlider, "BOTTOMLEFT", 0, -40)
-
 local heightSlider = CreateSlider(L["list height"], "listHeight")
-heightSlider:SetPoint("LEFT", widthSlider, "RIGHT", 24, 0)
 
+-- Schieberegler Positionen
+notifySlider:SetPoint("TOPLEFT", frameLabel, "BOTTOMLEFT", 0, -20)
+mainSlider:SetPoint("TOPLEFT", notifySlider, "BOTTOMLEFT", 0, -40)
+widthSlider:SetPoint("TOPLEFT", mainSlider, "BOTTOMLEFT", 0, -40)
+heightSlider:SetPoint("TOPLEFT", widthSlider, "BOTTOMLEFT", 0, -40)
+
+-- Funktion für Rücksetz-Schaltfläche
 local function OnResetFrames()
     notifySlider:SetValue(120)
     mainSlider:SetValue(100)
@@ -144,18 +161,18 @@ local function OnResetFrames()
 end
 
 local resetButton = CreateFrame("Button", nil, frame, "UIPanelButtonTemplate")
-resetButton:SetSize(150, 22) -- Beispielgröße, anpassen nach Bedarf
+resetButton:SetSize(150, 22)
 resetButton:SetText(L["reset frames"])
-resetButton:SetPoint("TOPLEFT", frameLabel, "BOTTOMLEFT", 8, -138)
+resetButton:SetPoint("BOTTOMLEFT", frame, "BOTTOMLEFT", 10, 10) -- Position am unteren Rand
 
 resetButton:SetScript("OnClick", function()
     addon:PopupShowConfirm(L["reset frames confirm"], OnResetFrames)
 end)
 
 local clearButton = CreateFrame("Button", nil, frame, "UIPanelButtonTemplate")
-clearButton:SetSize(150, 22) -- Beispielgröße, anpassen nach Bedarf
+clearButton:SetSize(150, 22)
 clearButton:SetText(L["clear all"])
-clearButton:SetPoint("LEFT", resetButton, "RIGHT", 8, 0)
+clearButton:SetPoint("LEFT", resetButton, "RIGHT", 10, 0) -- Position neben der Reset-Schaltfläche
 
 clearButton:SetScript("OnClick", function()
     addon:PopupShowConfirm(L["clear all confirm"], addon.Clear, addon)
